9de4d8135163791d46b87ff61db83ea5176ae3d3
sarahchou <chou.s@husky.neu.edu>
2017-04-29 22:16:07 -0400

    Added scoring.
    Added count and win text!

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
+	private int count;
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate () {
@@ -26,6 +33,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("Pickup")) {
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 13) {
+			winText.text = "You WIN!!!";
 		}
 	}
 
